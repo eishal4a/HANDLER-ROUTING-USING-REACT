c656bc72b4c0eff15d0bf223107df0ec9fabb8ce
eishal4a <eishal.cecos@gmail.com>
2025-09-17 15:41:21 +0500

    P
    P

--- handler-routing/src/App.js
+++ handler-routing/src/App.js
@@ -1,31 +1,30 @@
-// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function Home() {
-  return <h2>🏠 Home Page</h2>;
+  return <h2>Home Page</h2>;
 }
 
 function About() {
-  return <h2>ℹ️ About Page</h2>;
+  return <h2>About Page</h2>;
 }
 
 function Contact() {
-  return <h2>📞 Contact Page</h2>;
+  return <h2>Contact Page</h2>;
 }
 
 function App() {
   return (
     <Router>
       <div>
-        {/* NAVBAR */}
+     
         <nav style={{ padding: "10px", background: "#eee" }}>
           <Link to="/" style={{ margin: "10px" }}>Home</Link>
           <Link to="/about" style={{ margin: "10px" }}>About</Link>
           <Link to="/contact" style={{ margin: "10px" }}>Contact</Link>
         </nav>
 
-        {/* ROUTES */}
+       
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />

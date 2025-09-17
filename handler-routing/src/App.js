2036f56bfb02b53cb78fd3a2da3c7a072563c47e
Eishal <eishal.cecos@gmail.com>
2025-09-17 15:54:56 +0500

    Update App.js

--- handler-routing/src/App.js
+++ handler-routing/src/App.js
@@ -1,38 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function Home() {
-  return <h2>Home Page</h2>;
-}
-
-function About() {
-  return <h2>About Page</h2>;
-}
-
-function Contact() {
-  return <h2>Contact Page</h2>;
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
 
 function App() {
-  return (
-    <Router>
-      <div>
-     
-        <nav style={{ padding: "10px", background: "#eee" }}>
-          <Link to="/" style={{ margin: "10px" }}>Home</Link>
-          <Link to="/about" style={{ margin: "10px" }}>About</Link>
-          <Link to="/contact" style={{ margin: "10px" }}>Contact</Link>
-        </nav>
-
-       
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
-export default App;
+function Home() {
+  return <h1>🏠 Home Page</h1>;
+}
+function About() {
+  return <h1>ℹ️ About Page</h1>;
+}
